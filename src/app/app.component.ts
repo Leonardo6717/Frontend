e94780de568ac5c4ddc11f9c8746e3e4da23f8cc
Leonardo6717 <monteserrat.souza@gmail.com>
2025-04-23 17:16:06 -0300

    config vercel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,53 @@
-import { Component } from '@angular/core';
-import { Tarefa } from "./tarefa";
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface Tarefa {
+  _id?: string;
+  descricao: string;
+  concluido: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TODOapp';
   arrayDeTarefas: Tarefa[] = [];
-  apiURL : string;
- constructor(private http: HttpClient) {
-  this.apiURL = 'https://apitarefasleonardo237097-h8vf.onrender.com';
- this.READ_tarefas();
- }
- CREATE_tarefa(descricaoNovaTarefa: string) {
-  var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
-  this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
-    resultado => { console.log(resultado); this.READ_tarefas(); });
+  descricaoNovaTarefa = '';
+  readonly apiURL = 'https://apitarefasleonardo237097-h8vf.onrender.com/api';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.READ_tarefas();
   }
- 
- READ_tarefas() {
-  this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
-    resultado => this.arrayDeTarefas=resultado);
- }
- DELETE_tarefa(tarefaAserRemovida : Tarefa) {
-  var indice = this.arrayDeTarefas.indexOf(tarefaAserRemovida);
- var id = this.arrayDeTarefas[indice]._id;
- this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
- resultado => { console.log(resultado); this.READ_tarefas(); });
 
- }
+  CREATE_tarefa() {
+    if (!this.descricaoNovaTarefa.trim()) return;
+    const novaTarefa: Tarefa = { descricao: this.descricaoNovaTarefa, concluido: false };
+    this.http.post<Tarefa>(`${this.apiURL}/post`, novaTarefa).subscribe({
+      next: () => {
+        this.descricaoNovaTarefa = '';
+        this.READ_tarefas();
+      },
+      error: err => console.error('Erro ao criar tarefa:', err)
+    });
+  }
 
+  READ_tarefas() {
+    this.http.get<Tarefa[]>(`${this.apiURL}/getAll`).subscribe({
+      next: tarefas => this.arrayDeTarefas = tarefas,
+      error: err => console.error('Erro ao buscar tarefas:', err)
+    });
+  }
+
+  DELETE_tarefa(tarefa: Tarefa) {
+    if (!tarefa._id) return;
+    this.http.delete(`${this.apiURL}/delete/${tarefa._id}`).subscribe({
+      next: () => this.READ_tarefas(),
+      error: err => console.error('Erro ao excluir tarefa:', err)
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ItemComponent } from './item/item.component';
-import { ProdutoComponent } from './produto/produto.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ItemComponent,
-    ProdutoComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
